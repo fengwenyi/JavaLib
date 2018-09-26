e383aa396bff532acbf34a10ecaa6fc14d5f65b7
Wenyi Feng <xfsy_2015@163.com>
2018-09-26 18:27:58 +0800

    增加通用接口返回码

--- src/main/java/com/fengwenyi/javalib/result/DefaultReturnCode.java
+++ src/main/java/com/fengwenyi/javalib/result/DefaultReturnCode.java
@@ -17,14 +17,30 @@ import lombok.Getter;
 @Getter
 public enum DefaultReturnCode implements IReturnCode {
 
-    /** 初始化 */
-    ERROR_INIT(-1, "init..."),
+    //0
+    SUCCESS(0, "Success."),
 
-    /** 系统内部错误 */
-    ERROR_500(500, "(Error)程序出错"),
+    // 系统错误
+    ERROR_500(500, "(Error)程序出错。"),
+    ERROR_INIT(-1, "(Error)数据初始化。"),
+    ERROR_UNKNOWN(1000, "(Error)未知错误。"),
 
-    /** 成功 */
-    SUCCESS(0, "Success")
+    // 参数
+    ERROR_PARAM_NULL(1101, "(Error)参数不能为空。"),
+    ERROR_PARAM_REQUIRE_NULL(1102, "(Error)参数必填项不能为空。"),
+    ERROR_PARAM_WRONGFUL(1103, "(Error)参数不合法。"),
+
+    // 数据库
+    ERROR_DB_SAVE_FAIL(1201, "(Error)数据库保存数据失败。"),
+
+    // token
+    ERROR_TOKEN_NULL(1601, "(Error)token不能为空。"),
+    ERROR_TOKEN_WRONGFUL(1602, "(Error)token不合法。"),
+    ERROR_TOKEN_INCORRECT(1603, "(Error)token不匹配。"),
+
+    // 密钥
+    ERROR_KEY_NULL(1701, "(Error)密钥不能为空。"),
+    ERROR_KEY_AUTH_FAIL(1702, "(Error)密钥认证失败。")
     ;
 
     /** 返回码 */
