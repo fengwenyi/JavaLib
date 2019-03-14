40e409587cb34be07b8dc6a9df46940774cc8ee5
冯文议 <xfsy_2015@163.com>
2019-03-14 11:26:24 +0800

    增加日志级别常量

---
+++ src/test/java/com/fengwenyi/javalib/constant/TestLog.java
@@ -0,0 +1,15 @@
+package com.fengwenyi.javalib.constant;
+
+import org.junit.Test;
+
+/**
+ * @author Erwin
+ * @date 2019-03-14 11:25
+ */
+public class TestLog {
+
+    @Test
+    public void test() {
+        //LogLevel.ALL;
+    }
+}
