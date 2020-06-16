16b7db10355e36a9577811c0c5119c5ea475a07a
Erwin <xfsy_2015@163.com>
2020-06-16 21:35:24 +0800

    2.0.5 新增JsonUtils

---
+++ src/main/java/com/fengwenyi/javalib/util/JsonUtils.java
@@ -0,0 +1,21 @@
+package com.fengwenyi.javalib.util;
+
+/**
+ * Json工具类
+ * @author Erwin Feng
+ * @since 2020/6/16
+ */
+public class JsonUtils {
+
+    // 对象转json
+    public static String tojson() {
+        return "";
+    }
+
+    // list转json
+
+    // map转json
+
+    // json转对象
+
+}
