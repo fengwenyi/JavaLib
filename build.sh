7e95924778e35f61f2caf7e1e3d0a6630dedf0e2
ErwinFeng <xfsy_2015@163.com>
2024-08-04 19:28:27 +0800

    3.0.0 release

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 version=`awk '/<version>[^<]+<\/version>/{gsub(/<version>|<\/version>/,"",$1);print $1;exit;}' pom.xml`
 echo $version
-mvn clean deploy -P sonatype-oss-release -DskipTests
+# mvn clean deploy -P sonatype-oss-release -DskipTests
+mvn clean deploy -P release
 git tag -a $version -m "v$version"
 git push origin $version
